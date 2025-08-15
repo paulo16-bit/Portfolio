fec85eea90537af589dd1b19fd6ef8cf0f827d73
paulo16-bit <77926556+paulo16-bit@users.noreply.github.com>
2025-08-15 10:53:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const sections = document.querySelectorAll('section[id]');
 const navLinks = document.querySelectorAll('.nav__link');
+const navToggle = document.getElementById("nav-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+// Abrir / Fechar menu
+navToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("show-menu");
+});
+
+// Fechar menu ao clicar em um link
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+});
+
 
 window.addEventListener('scroll', () => {
   let current = '';
