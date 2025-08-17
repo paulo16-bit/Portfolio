4dd023a192c7d1e8ed7a6eda79537fd7ba0666b5
paulo16-bit <77926556+paulo16-bit@users.noreply.github.com>
2025-08-17 13:55:45 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const navLinks = document.querySelectorAll('.nav__link');
 const navToggle = document.getElementById("nav-toggle");
 const navMenu = document.getElementById("nav-menu");
 
-// Abrir / Fechar menu
+
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("show-menu");
 });
 
-// Fechar menu ao clicar em um link
+
 navLinks.forEach(link => {
   link.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
@@ -34,3 +34,20 @@ window.addEventListener('scroll', () => {
   });
 });
 
+function sendEmail(event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const subject = document.getElementById("subject").value;
+  const message = document.getElementById("message").value;
+
+  const destinatario = "ppauloaandre80@gmail.com";
+
+  const assunto = encodeURIComponent(subject);
+  const corpo = encodeURIComponent(
+    `Nome: ${name}\nEmail: ${email}\n\nMensagem:\n${message}`
+  );
+
+  window.location.href = `mailto:${destinatario}?subject=${assunto}&body=${corpo}`;
+}
